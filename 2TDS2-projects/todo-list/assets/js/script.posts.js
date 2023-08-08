60e9a570cfd6b49cd8bddd81d6bc6cb5884e44b6
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-08 16:22:42 -0300

    feat: create edit post function

--- 2TDS2-projects/todo-list/assets/js/script.posts.js
+++ 2TDS2-projects/todo-list/assets/js/script.posts.js
@@ -1,28 +1,42 @@
 const posts = [];
-function savePost(){
+let postIndex = -1;
+function savePost() {
     const tittle = document.getElementById("tittle").value;
-    const category= document.getElementById("category").value;
+    const category = document.getElementById("category").value;
     const resume = document.getElementById("resume").value;
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
-    if(tittle && category && resume && author && date){
-    storePost(tittle, category, resume, author, date)
-    cleanFields();
-    showPosts();
-}else{
-    alert("Preencha todos os campos!");
+    if (tittle && category && resume && author && date) {
+        if (postIndex == -1) {
+            storePost(tittle, category, resume, author, date)
+            cleanFields();
+            showPosts();
+        } else {
+            posts[postIndex] = {
+                tittle,
+                category,
+                resume,
+                author,
+                date,
+            };
+        }
+        cleanFields();
+        showPosts();
+        postIndex = -1;
+    } else {
+        alert("Preencha todos os campos!");
+    }
 }
-}
-function cleanFields(){
+function cleanFields() {
     document.getElementById("tittle").value = "";
     document.getElementById("category").value = "";
     document.getElementById("resume").value = "";
     document.getElementById("author").value = "";
     document.getElementById("date").value = "";
 }
-function storePost(tittle, category, resume, author, date){
+function storePost(tittle, category, resume, author, date) {
     const post = {
-        tittle , 
+        tittle,
         category,
         resume,
         author,
@@ -31,9 +45,9 @@ function storePost(tittle, category, resume, author, date){
     posts.push(post);
     console.log(posts)
 }
-function showPosts(){
+function showPosts() {
     let showContent = "";
-    posts.forEach((post , index) =>{
+    posts.forEach((post, index) => {
         showContent += `
         <div class="itemPost">
         <h2>${post.tittle}</h2>
@@ -45,6 +59,15 @@ function showPosts(){
         <button onclick="deletePost(${index})">Excluir</button>
         </div>
         `;
-    } );
+    });
     document.getElementById("list").innerHTML = showContent;
+}
+function editPost(index) {
+    const post = posts[index];
+    document.getElementById("tittle").value = post.tittle;
+    document.getElementById("category").value = post.category;
+    document.getElementById("resume").value = post.resume;
+    document.getElementById("author").value = post.author;
+    document.getElementById("date").value = post.date;
+    postIndex = index;
 }
