1eff5997f3a761692230ba07536aa50312526483
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-08 14:30:04 -0300

    feat: create store post function

--- 2TDS2-projects/todo-list/assets/js/script.posts.js
+++ 2TDS2-projects/todo-list/assets/js/script.posts.js
@@ -1,10 +1,16 @@
+const posts = [];
 function savePost(){
     const tittle = document.getElementById("tittle").value;
     const category= document.getElementById("category").value;
     const resume = document.getElementById("resume").value;
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
+    if(tittle && category && resume && author && date){
+    storePost(tittle, category, resume, author, date)
     cleanFields()
+}else{
+    alert("Preencha todos os campos!");
+}
 }
 function cleanFields(){
     document.getElementById("tittle").value = " ";
@@ -12,4 +18,15 @@ function cleanFields(){
     document.getElementById("resume").value = " ";
     document.getElementById("author").value = " ";
     document.getElementById("date").value = " ";
+}
+function storePost(tittle, category, resume, author, date){
+    const post = {
+        tittle , 
+        category,
+        resume,
+        author,
+        date,
+    };
+    posts.push(post);
+    console.log(posts)
 }
