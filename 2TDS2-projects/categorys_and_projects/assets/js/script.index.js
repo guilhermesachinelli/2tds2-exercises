a3a06d9918513a22606fa5fe4225ca6108959819
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-17 10:16:42 -0300

    feat: add Products class

--- 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
+++ 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
@@ -5,3 +5,11 @@ class Category{
         this.products = [];
     }
 }
+class Products{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
