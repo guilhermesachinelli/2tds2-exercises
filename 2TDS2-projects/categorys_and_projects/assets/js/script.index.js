53fa4b59881c0d702b28db7e121296efe52a93ab
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-17 17:06:46 -0300

    feat: add displayCategoriesAndProducts function

--- 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
+++ 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
@@ -19,7 +19,7 @@ class CategoryService {
         this.nextCategoryId = 1;
     }
     addCategory(name) {
-        const id = this.nextCategoryId ++;
+        const id = this.nextCategoryId++;
         const category = new Category(id, name);
         this.categories.push(category);
     }
@@ -30,7 +30,7 @@ class ProductService {
         this.nextProductId = 0;
     }
     addProduct(name, price, category) {
-        const id = this.nextProductId ++;
+        const id = this.nextProductId++;
         const product = new Products(id, name, price, category);
         category.products.push(product);
         this.products.push(product);
@@ -42,18 +42,52 @@ const productList = new ProductService();
 function createCategory() {
     const categorieName = document.getElementById("categoryName").value;
     categoryList.addCategory(categorieName);
+    displayCategoriesAndProducts()
+    clearFormFields();
     console.log(categoryList.categories);
 }
-function createProduct(){
+function createProduct() {
     const productName = "Bolo";
     const productPrice = "20";
     const productCategory = categoryList.categories[0];
     productList.addProduct(productName, productPrice, productCategory);
     console.log(productList.products);
 }
-function clearFormFields(){
+function clearFormFields() {
     document.getElementById("categoryName").value = "";
     document.getElementById("productName").value = "";
     document.getElementById("productPrice").value = "";
     document.getElementById("productCategory").value = "";
+}
+function displayCategoriesAndProducts() {
+    let content = "";
+    categoryList.categories.forEach((category) => {
+        content += `
+        <li>
+        <div class="categoriesList">
+        <span><b>Categoria:</b>${category.name}</span>
+        <div>
+        <button class="editButton">Editar</button>
+        <button class="deleteButton">Remover</button>
+        </div>
+        </div>
+        <ul class="productListByCategory>`;
+        category.products.forEach((product) =>{
+            content+=`
+            <li>
+            <div class="productsList">
+            <span><b>Produto:</b> ${product.name} - <b>Preço:</b></span>
+            <div>
+            <button class="editButton">Editar</button>
+            <button class="deleteButton">Remover</button>
+            </div>
+            </div>
+            `;
+        });
+        content +=`
+        </ul>
+        </li>
+        `
+    });
+    document.getElementById("categoriesList").innerHTML = content;
 }
