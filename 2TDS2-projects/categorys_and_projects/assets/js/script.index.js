89c336f7a85776e7fe3d1557a2e486e6299d1363
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-17 10:30:53 -0300

    feat: add class CategoryService and method

--- 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
+++ 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
@@ -13,3 +13,14 @@ class Products{
         this.category = category;
     }
 }
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+    addCategory(name){
+        const id = this.nextCategoryId ++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+}
