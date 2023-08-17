af28c947faa9848e16914b6b909f3cb2345f70b9
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-17 10:07:13 -0300

    feat: crate categories and products

--- 2TDS2-projects/harrypotter-js/camera-secreta/script.js
+++ 2TDS2-projects/harrypotter-js/camera-secreta/script.js
@@ -6,7 +6,7 @@ class CamaraSecreta{
         this.verifyPwd = (pwdInput) =>{
             return pwdInput == _pwd; //True or False
         };
-        this.accessToChamber = () => {
+        this.accessChamber = () => {
             let pwdInput = prompt("Digite a senha");
             if(this.verifyPwd(pwdInput)){
                 console.log(`Bem vindo a Camara Secreta ${this.name}`)
@@ -14,5 +14,17 @@ class CamaraSecreta{
                 console.log("Acesso negado trouxa!")
             }
         }
+    };
+    //Metodos pubicos
+    showName(){
+        console.log(`Nome do Bruxo: ${this.name}`)
     }
+    
 }
+const myAccess1 = new CamaraSecreta('Harry Potter', 'senha123')
+
+//Metodo publico
+myAccess1.showName();//top
+myAccess1.name = 'HP';
+myAccess1.pwd = '123';
+myAccess1.accessChamber();
