c9220cc6edcbad08ba17ae90209800343ae92714
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-08-17 14:26:16 -0300

    feat: initiate html

--- 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
+++ 2TDS2-projects/categorys_and_projects/assets/js/script.index.js
@@ -1,43 +1,53 @@
-class Category{
-    constructor(id, name){
-        this.id  = id;
+class Category {
+    constructor(id, name) {
+        this.id = id;
         this.name = name;
         this.products = [];
     }
 }
-class Products{
-    constructor(id, name, price, category){
+class Products {
+    constructor(id, name, price, category) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
     }
 }
-class CategoryService{
-    constructor(){
+class CategoryService {
+    constructor() {
         this.categories = [];
         this.nextCategoryId = 0;
     }
-    addCategory(name){
+    addCategory(name) {
         const id = this.nextCategoryId ++;
         const category = new Category(id, name);
         this.categories.push(category);
     }
 }
-class ProductService{
-    constructor(){
+class ProductService {
+    constructor() {
         this.products = [];
         this.nextProductId = 0;
     }
-    addProduct(name, price, category){
+    addProduct(name, price, category) {
         const id = this.nextProductId ++;
-        const product = new this.Products(id, name, price, category);
+        const product = new Products(id, name, price, category);
+        category.products.push(product);
         this.products.push(product);
     }
 }
 const categoryList = new CategoryService();
-function createCategory(){
+const productList = new ProductService();
+
+function createCategory() {
     const categorieName = "Doce";
     categoryList.addCategory(categorieName);
     console.log(categoryList.categories);
+}
+function createProduct(){
+    const productName = "Bolo";
+    const productPrice = "20";
+    const productCategory = categoryList.categories[0];
+    productList.addProduct(productName, productPrice, productCategory);
+    console.log(productList.products);
 }
